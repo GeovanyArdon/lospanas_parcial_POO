2ffb3e3d58fae034fcc2fc2eaf941b9e1a33fbd5
Gabriel <fernandomurcia451@gmail.com>
2020-04-19 20:27:22 -0600

    add: Exeptions

---
+++ Ejercicio Parcial/src/com/GJAV/x00118618/AlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.FGMS.x00007419;
+
+
+    public class AlreadyExistException extends  Exception {
+        public AlreadyExistException(String message) {
+            super(message);
+        }
+    }
+
---
+++ Ejercicio Parcial/src/com/GJAV/x00118618/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.FGMS.x00007419;
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
